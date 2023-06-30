c927ac012c77c7e5794e4a4ea011760fe6c4045b
Aiswariyakr <kraiswariya77@gmail.com>
2023-06-30 16:22:02 +0530

    Completed the 3 Exercises

--- src/app/exercises/exercise2/exercise2.module.ts
+++ src/app/exercises/exercise2/exercise2.module.ts
@@ -2,14 +2,35 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Exercise2Component } from './exercise2.component';
 
+import { FormsModule } from '@angular/forms';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
+import { InvertedParentComponent } from './inverted-parent/inverted-parent.component';
+import { InvertedChildComponent } from './inverted-child/inverted-child.component';
+import { RouterModule } from '@angular/router';
+
 
 
 @NgModule({
   declarations: [
-    Exercise2Component
+    Exercise2Component,
+    ParentComponent,
+    ChildComponent,
+    InvertedParentComponent,
+    InvertedChildComponent,
+   
   ],
   imports: [
+    FormsModule,
     CommonModule,
+    RouterModule.forChild([
+    {
+       path: '',
+       component: Exercise2Component,
+  
+      },
+  
+    ]),
   ]
 })
 export class Exercise2Module { }
